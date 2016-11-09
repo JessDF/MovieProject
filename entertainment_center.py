cfd1bfd570ce0cb86de084d0959d193e01e5f7fb
Jessie Dowding <jdowding@csumb.edu>
2016-11-09 09:17:50 -0800

    Cleaned up code

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,44 +1,40 @@
 import media
 import fresh_tomatoes   #links up the fresh_tomatoes and media pages
 
+#toy_story is an instance of the class, "Movie"
 toy_story = media.Movie("Toy Story",
                         "A story about a boy and his toys taht come to life",
                         "http://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg",
-                        "https://www.youtube.com/watch?v=vwyZH85NQC4")  #toy_story is an instance of the class, "Movie"
-#print(toy_story.storyline)
+                        "https://www.youtube.com/watch?v=vwyZH85NQC4")  
 
 avatar = media.Movie("Avatar",
                         "A marine on an alien planet",
-                        "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",
+                        "https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg",# noqa
                         "https://www.youtube.com/watch?v=d1_JBMrrYw8")
-#print(avatar.storyline)
-#avatar.show_trailer()
 
 mulan = media.Movie("Mulan",
-                        "Mulan disguises herself as a male soldier to take her father's place in the army",
-                        "http://www.gstatic.com/tv/thumb/movieposters/21118/p21118_p_v8_aa.jpg",
+                        "Mulan disguises herself as a man to take her father's place in the army",
+                        "http://www.gstatic.com/tv/thumb/movieposters/21118/p21118_p_v8_aa.jpg", # noqa
                         "https://www.youtube.com/watch?v=MsAniqGowKE")
-#mulan.show_trailer()
-
 
 ratatouille = media.Movie("Ratatouille ",
                              "A rat is a chef in Paris",
-                             "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg",
+                             "https://upload.wikimedia.org/wikipedia/en/5/50/RatatouillePoster.jpg", # noqa
                              "https://www.youtube.com/watch?v=c3sBBRxDAqk")
 
 aristocats = media.Movie("The Aristocats",
-                             "In the heart of Paris, a kind and eccentric millionairess wills her entire estate to Duchess, her high-society cat, and her three little kittens",
-                             "http://www.gstatic.com/tv/thumb/movieposters/19375/p19375_p_v8_af.jpg",
+                             "In the heart of Paris, a millionairess wills her entire estate to her cats",
+                             "http://www.gstatic.com/tv/thumb/movieposters/19375/p19375_p_v8_af.jpg", # noqa
                              "https://www.youtube.com/watch?v=wjA5LWNUPDY")
 
 brave = media.Movie("Brave",
                              "Princess Merida seeks to control her own destiny in Disney and Pixar's Brave.",
-                             "http://t0.gstatic.com/images?q=tbn:ANd9GcSuKUcpKMfuY1En5kWsJB2Xe-dqu-yYrQM6KTps3-Mti04lgmSY",
+                             "http://t0.gstatic.com/images?q=tbn:ANd9GcSuKUcpKMfuY1En5kWsJB2Xe-dqu-yYrQM6KTps3-Mti04lgmSY", # noqa
                              "https://www.youtube.com/watch?v=TEHWDA_6e3M")
-
-movies = [toy_story, avatar, mulan, ratatouille, aristocats, brave] #creates an array of the "Movie" class instances
-fresh_tomatoes.open_movies_page(movies)   #takes the array movies and addes them to webpage
-#print(media.Movie.VALID_RATINGS)
+#Below: creates an array of the "Movie" class instances
+movies = [toy_story, avatar, mulan, ratatouille, aristocats, brave] 
+#Below: takes the array movies and addes them to webpage
+fresh_tomatoes.open_movies_page(movies)   
 print(media.Movie.__doc__)  #prints documentation of the class Movie
 print(media.Movie.__name__) #prints class name
 print(media.Movie.__module__)   #prints the module we imported/ name of file
